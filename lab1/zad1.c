ad38d8dc9df0071478ad4662f2130f2b28b057ba
Ivan Juricic <ivanjuricic97@gmail.com>
2021-10-26 14:47:28 +0200

    Add lab1 initial commit

---
+++ lab1/zad1.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[]) {
+
+    printf("Hello there");
+    return 0;
+}
