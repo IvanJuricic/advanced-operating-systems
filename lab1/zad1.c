a66ea03166d2792c69009ae4299fdbbe6c536a89
Ivan Juricic <ivanjuricic97@gmail.com>
2021-10-26 15:06:01 +0200

    Minor changes

--- lab1/zad1.c
+++ lab1/zad1.c
@@ -7,8 +7,47 @@
 #include <sys/ipc.h>
 #include <sys/wait.h>
 
+void carousselProcess() {
+
+}
+
+void visitorProcess() {
+
+}
+
 int main(int argc, char *argv[]) {
 
-    printf("Hello there");
+    int processId;
+    printf("\n\tCreating caroussel process...\n");
+
+    switch (processId = fork()) {
+            case -1:
+                printf("\t\nUnable to create new process!\n");
+                exit(1);
+                
+            case 0:
+                carousselProcess();
+                exit(0);
+            
+            default:
+                break;
+    }
+
+    for(int i = 0; i < 8; i++) {
+        switch (processId = fork()) {
+            case -1:
+                printf("\t\nUnable to create new process!\n");
+                exit(1);
+                
+            case 0:
+                printf("\n\tCreated %d. visitor process!\n", i+1);
+                visitorProcess();
+                exit(0);
+            
+            default:
+                break;
+        }
+    }
+
     return 0;
 }
