3a32f074770f803fc61ca6ccca5c282736560a95
IvanJuricic <ivanjuricic97@gmail.com>
2021-11-09 18:22:15 +0100

    Add carousel code

--- lab1/carousel.py
+++ lab1/carousel.py
@@ -10,13 +10,15 @@ class Carousel:
 
     def __init__(self) -> None:
         self.visitors = []
-        self.process = None
         self.max_visitors = 4
         self.curr_visitors = 0
         self.curr_visitors_ids = []
         
         self.in_queue = mp.Queue()
-        self.out_queue = mp.Queue()
+        
+        self.out_sit_queue = mp.Queue()
+        self.out_get_up_queue = mp.Queue()
+
         self.sync_queue = mp.Queue()
 
     def run(self):
@@ -28,120 +30,121 @@ class Carousel:
 
         for i in range(0, len(self.visitors)):
             print(Fore.CYAN + "[CAROUSEL]: Visitor created with id => {}".format(self.visitors[i].get_id ))
-            self.visitors[i].create_and_go(self.in_queue, self.out_queue, self.sync_queue)
-
+            self.visitors[i].create_and_go(self.in_queue, self.out_sit_queue, self.out_get_up_queue, self.sync_queue)
+            
+        #sleep(1)
+        
         while self.visitors:
             msg, id = self.in_queue.get()
             self.curr_visitors_ids.append(id)
-            print(Fore.WHITE + "\n\t[CAROUSEL]: Received msg => {}\n".format(msg))
+            print(Fore.WHITE + "\n\t[CAROUSEL]: Received msg => {} from visitor {}\n".format(msg, id))
             if msg == "I wanna ride":
                 
-                self.out_queue.put(("Sit down", id))
+                self.out_sit_queue.put(("Sit down", id))
                 self.curr_visitors += 1
+
                 sleep(0.2)
+                
                 if self.curr_visitors == self.max_visitors:
                     
+                    #Carousel.carousel_full = True
+                    print("\n\t\t\t\t[CAROUSEL]: Visitor IDs that are on carousel => \n", self.curr_visitors_ids)
                     print(Fore.GREEN + "\n\t\t\t\t[CAROUSEL]: Carousel active now....\n")
                     sleep(random.randint(5,10))
                     print(Fore.RED + "\n\t\t\t\t[CAROUSEL]: Carousel stopped now!\n")
                     
+
                     for i in range(self.max_visitors):
-                        self.out_queue.put(("Get up", self.curr_visitors_ids.pop()))
+                        #sleep(1)
+                        #Carousel.visitors_sitting_down -= 1
+                        id = self.curr_visitors_ids.pop()
+                        self.out_get_up_queue.put(("Get up", id))
                     
                     self.curr_visitors = 0
+                    #Visitor.carousel_full = False
+                    
 
-            elif msg == "[VISITOR]: I am done":
-                self.visitors.pop(id)
+            elif msg == "I am done":
+                self.curr_visitors_ids.pop()
+                self.visitors.pop()
 
 
 
 
 class Visitor:
+
+    count = 0
+
     def __init__(self, id) -> None:
         self._id = id
         self.process = None
+        self.waiting = False
 
     @property
     def get_id(self):
         return self._id
 
-    def create_and_go(self, input, output, sync):
-        self.process = mp.Process(target=self.run, args=(input, output, sync))
+    def request_sit(self, queue_to_carousel : mp.Queue, queue_sit_from_carousel : mp.Queue, sync_queue : mp.Queue):
+        
+        queue_to_carousel.put(("I wanna ride", self.get_id))
+        msg, id = queue_sit_from_carousel.get()
+        
+        #tmp = id
+        #if tmp == 7: tmp = 0
+        #else: tmp += 1
+
+        print(Fore.LIGHTBLUE_EX + "\n[VISITOR]: Message from queue => {}\tID => {}".format(msg, id))
+
+        if msg == "Sit down" and id == self.get_id:
+            print(Fore.LIGHTYELLOW_EX + "\n[VISITOR]: Visitor {} sat down\n".format(self.get_id))
+            self.count += 1
+            self.waiting = True
+            #sync_queue.put(tmp)
+
+    def wait_get_up(self, queue_get_up_from_carousel : mp.Queue, sync_queue : mp.Queue):
+        
+        msg, id = queue_get_up_from_carousel.get()
+        print(Fore.LIGHTBLUE_EX + "\n[VISITOR]: Message from queue => {}\tID => {}".format(msg, id))
+        
+        if msg == "Get up" and id == self.get_id:
+            print("\n\t[VISITOR]: Visitor {} got up and left the carousel\n".format(self.get_id))
+            self.waiting = False
+            self.count -= 1
+            #sync_queue.put(tmp)
+
+    def create_and_go(self, input, output_sit, output_get_up, sync):
+        self.process = mp.Process(target=self.run, args=(input, output_sit, output_get_up, sync))
         self.process.start()
 
-    def run(self, queue_to_carousel : mp.Queue, queue_from_carousel : mp.Queue, sync_queue : mp.Queue):
+    def run(self, queue_to_carousel : mp.Queue, queue_sit_from_carousel : mp.Queue, queue_get_up_from_carousel: mp.Queue, sync_queue : mp.Queue):
+        
         sleep(1)
-        for _ in range(3):
 
+        for i in range(3):
+            x = i + 1
             while True:
-                sleep(random.randint(2, 3))
+                sleep(randint(1,4))
+
                 sync_id = sync_queue.get()
 
-                print(Fore.WHITE + "\nFROM SYNC ==> ", sync_id)
-                print(Fore.WHITE + "\nVisitor ==> ", self.get_id)
-
                 tmp = sync_id
-
                 if tmp == 7: tmp = 0
                 else: tmp += 1
-                
+
                 if sync_id == self.get_id:
                     
-                    print(Fore.MAGENTA + "\n\t\t[VISITOR]: Visitor {} has the token!".format(self.get_id))
-                    queue_to_carousel.put(("I wanna ride", self.get_id))
+                    self.request_sit(queue_to_carousel, queue_sit_from_carousel, sync_queue)
+                    sync_queue.put(tmp)
 
-                    while True:
-                            
-                        cmd, id = queue_from_carousel.get()
-                        sync_queue.put(tmp)
-                        print("\n[VISITOR]: Token back in circulation\n")
 
-                        if id == self.get_id:
-                            print("\n\t[VISITOR]: Visitor {} sat down\n".format(self.get_id))
-                            break
-                        else:
-                            queue_from_carousel.put((cmd, sync_id))
-                            break
 
-                    break
+                    if self.count == x:
+                        self.wait_get_up(queue_get_up_from_carousel, sync_queue)
+                        break
 
                 else:
-                    sleep(1)
                     sync_queue.put(sync_id)
-
-            print("\n[VISITOR]: Visitor {} waiting to get up...\n".format(self.get_id))
-
-            while True:
-                sleep(random.randint(2, 3))
-                sync_id = sync_queue.get()
                 
-                print(Fore.WHITE + "\nFROM SYNC ==> ", sync_id)
-                print(Fore.WHITE + "\nVisitor ==> ", self.get_id)
-
-                tmp = sync_id
-
-                if tmp == 7: tmp = 0
-                else: tmp += 1
-
-                if sync_id == self.get_id:
-
-                    print(Fore.MAGENTA + "\n\t\t[VISITOR]: Visitor {} has the token!".format(self.get_id))
-
-                    while True:
-                        
-                        cmd, id = queue_from_carousel.get()
-                        sync_queue.put(tmp)
-                        print("\n[VISITOR]: Token back in circulation\n")
-
-                        if id == self.get_id:
-                            if cmd == "Get up":
-                                print("\n\t[VISITOR]: Visitor {} got up and left the carousel\n".format(self.get_id))
-                                break
-                            else:
-                                queue_from_carousel.put((cmd, sync_id))
-                                break
-                else:
-                    sync_queue.put(sync_id)
 
         queue_to_carousel.put(("I am done", self.get_id))
         print("\n\t\t[VISITOR]: Visitor {} is done riding\n".format(self.get_id))
